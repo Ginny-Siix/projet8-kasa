9307ca1cb2c02b4a1c09d8aa521571589a630587
Ginny-Siix <127752611+Ginny-Siix@users.noreply.github.com>
2023-11-14 20:47:43 +0100

    ajout échapé apostrophe error page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,12 +1,14 @@
 import "./error404.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Error404() {
   return (
     <main>
       <h1 className="h1_error">404</h1>
-      <h2>Oups! La page que vous demandez nexiste pas.</h2>
-      <Link to='/' className="p_error">Retourner sur la page daccueil</Link>
+      <h2>Oups! La page que vous demandez n&#8217;existe pas.</h2>
+      <Link to="/" className="p_error">
+        Retourner sur la page daccueil
+      </Link>
     </main>
   );
 }
